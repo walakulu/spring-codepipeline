ad6e805258870aa02da84a62dccffbdfdee768c3
Hasitha Maduranga <Hasitha.Ma@creativesoftware.com>
2021-06-23 17:08:24 +0530

    add base path

--- src/main/java/com/example/springcodepipeline/SpringCodepipelineApplication.java
+++ src/main/java/com/example/springcodepipeline/SpringCodepipelineApplication.java
@@ -11,7 +11,7 @@ public class SpringCodepipelineApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World with Hasitha";
+		return "Hello Docker World with Micro-Service-1";
 	}
 
 
